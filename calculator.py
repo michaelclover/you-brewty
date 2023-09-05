a9c4d1aa41a5852ff33d4725ebb5511fc3a08884
michaelclover <michaelac2030@gmail.com>
2023-09-05 20:43:55 +0100

    developed the UI some more while learning customtkinter

--- calculator.py
+++ calculator.py
@@ -1,63 +1,31 @@
 import recipe
 import window
     
+def output_recipe(recipe):
+    print(f"\nFermentables weight: {recipe.fermentables_weight():.2f}lbs\n\
+Target water-to-grist ratio: {recipe.target_water_grist_ratio} ltr/kg\n\
+Initial volume: {recipe.initial_volume:.2f}gals\n\
+Mash volume: {recipe.mash_volume():.2f}gals\n\
+Sparge volume: {recipe.sparge_volume():.2f}gals\n\
+Pre-boil volume: {recipe.pre_boil_volume():.2f}gals\n\
+Post-boil volume: {recipe.post_boil_volume():.2f}gals\n\
+Estimated pre-boil og: {recipe.pre_boil_og():.3f}\n\
+Estimated post-boil og: {recipe.post_boil_og():.3f}\n\
+Estimated fg: {recipe.final_gravity():.3f}\n\
+ABV: {recipe.estimate_abv():.2f}%\n\
+IBU: {recipe.tinseth_ibu():.2f}\n\
+MCU: {recipe.malt_colour_units():.2f}\n\
+SRM: {recipe.morey_srm():.2f}\n")
+
 def main():
 
     # load our recipe.
     sample_recipe = recipe.Recipe()
     sample_recipe.load_file("./recipes/SMaSH pale ale.json")
 
-    # calculate the mash volume based on the desired water-to-grist ratio.
-    mash_volume = sample_recipe.mash_volume()
+    output_recipe(sample_recipe)
 
-    # calculate the sparge volume based on the initial volume minus the mash volume.
-    sparge_volume = sample_recipe.sparge_volume()
-
-    # given a mash volume (in us gallons) and weight of fermentables (in lbs), 
-    # calculate the remaining liquid after grain absorption.
-    pre_boil_volume = sample_recipe.pre_boil_volume()
-
-    # calculate the post boil volume, accounting for water loss to evaporation.
-    post_boil_volume = sample_recipe.post_boil_volume()
-    
-    # given lbs of fermentables, their potential, the final fermenter volume,
-    # an efficiency factor based on how much sugar the system can extract
-    # from the fermentables, and the attentuation of the brewing yeast used,
-    # estimate the final ABV content.
-    abv = sample_recipe.estimate_abv()
-
-    # calculate the pre-boil og.
-    pre_boil_og = sample_recipe.pre_boil_og()
-
-    # calculate the post-boil og.
-    post_boil_og = sample_recipe.post_boil_og()
-
-    # calculate the final gravity.
-    fg = sample_recipe.final_gravity()
-
-    # calculate the tinseth bittering units.
-    ibu = sample_recipe.tinseth_ibu()
-
-    # calculate the malt colour units and the Morey standard reference method.
-    mcu = sample_recipe.malt_colour_units()
-    srm = sample_recipe.morey_srm()
-
-    print(f"\nFermentables weight: {sample_recipe.fermentables_weight():.2f}lbs\n\
-Target water-to-grist ratio: {sample_recipe.target_water_grist_ratio} ltr/kg\n\
-Initial volume: {sample_recipe.initial_volume:.2f}gals\n\
-Mash volume: {mash_volume:.2f}gals\n\
-Sparge volume: {sparge_volume:.2f}gals\n\
-Pre-boil volume: {pre_boil_volume:.2f}gals\n\
-Post-boil volume: {post_boil_volume:.2f}gals\n\
-Estimated pre-boil og: {pre_boil_og:.3f}\n\
-Estimated post-boil og: {post_boil_og:.3f}\n\
-Estimated fg: {fg:.3f}\n\
-ABV: {abv:.2f}%\n\
-IBU: {ibu:.2f}\n\
-MCU: {mcu:.2f}\n\
-SRM: {srm:.2f}\n")
-
-    win = window.Window()
+    win = window.Window(sample_recipe)
     win.mainloop()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -5,44 +5,65 @@ ctk.set_default_color_theme("green")
 
 class Window(ctk.CTk):
 
-    def __init__(self):
+    def __init__(self, recipe):
 
         super().__init__()
 
+        self.recipe = recipe
+
         self.title("You Brewty!")
+        self.attributes('-zoomed', True)
 
         self.grid_rowconfigure((0, 1, 2, 3), weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure((0), weight=1)
         
-        configuration_frame = ctk.CTkScrollableFrame(self, fg_color=self.cget("bg"))
-        configuration_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.configuration_frame = ctk.CTkFrame(self, fg_color=self.cget("bg"), border_color="white", border_width=1)
+        self.configuration_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.configuration_frame.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
 
-        configuration_label = ctk.CTkLabel(master=configuration_frame, text="Configuration")
-        configuration_label.grid(row=0, column=0, pady=(0, 20))
+        configuration_label = ctk.CTkLabel(master=self.configuration_frame, text="Configuration")
+        configuration_label.grid(row=0, column=3, sticky="nsew", pady=5)
 
-        fermentables_frame = ctk.CTkScrollableFrame(self, fg_color=self.cget("bg"))
-        fermentables_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        self.fermentables_frame = ctk.CTkFrame(self, fg_color=self.cget("bg"), border_color="white", border_width=1)
+        self.fermentables_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+        self.fermentables_frame.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
 
-        fermentables_label = ctk.CTkLabel(master=fermentables_frame, text="Fermentables")
-        fermentables_label.grid(row=0, column=0, pady=(0, 20))
+        fermentables_label = ctk.CTkLabel(master=self.fermentables_frame, text="Fermentables", anchor="w")
+        fermentables_label.grid(row=0, column=2, sticky="nsew", pady=5)
 
-        fermentables_entry = ctk.CTkEntry(fermentables_frame, placeholder_text="fermentable")
-        fermentables_entry.grid(row=1, column=0)
+        self.fermentables_entry = ctk.CTkEntry(self.fermentables_frame, placeholder_text="fermentable")
+        self.fermentables_entry.grid(row=1, column=0, sticky="nsew", padx=10)
 
-        fermentables_button = ctk.CTkButton(fermentables_frame, text="add")
-        fermentables_button.grid(row=1, column=1, padx=10)
+        self.fermentables_frame_scroll = ctk.CTkScrollableFrame(master=self.fermentables_frame, border_color="white", border_width=1)
+        self.fermentables_frame_scroll.grid(row=2, column=0, sticky="nsew", padx=10, pady=(5, 0))
+        self.fermentables_frame_scroll.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        self.rowno = 0
 
-        hops_frame = ctk.CTkScrollableFrame(self, fg_color=self.cget("bg"))
-        hops_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        fermentables_button = ctk.CTkButton(self.fermentables_frame, text="add", command=self.button_add_fermentable)
+        fermentables_button.grid(row=1, column=1, sticky="nsew")
 
-        hops_label = ctk.CTkLabel(master=hops_frame, text="Hops")
-        hops_label.grid(row=0, column=0, pady=(0, 20))
+        self.hops_frame = ctk.CTkFrame(self, fg_color=self.cget("bg"), border_color="white", border_width=1)
+        self.hops_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        self.hops_frame.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
 
-        yeast_frame = ctk.CTkScrollableFrame(self, fg_color=self.cget("bg"))
-        yeast_frame.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+        hops_label = ctk.CTkLabel(master=self.hops_frame, text="Hops")
+        hops_label.grid(row=0, column=3, sticky="nsew", pady=5)
 
-        yeast_label = ctk.CTkLabel(master=yeast_frame, text="Yeast")
-        yeast_label.grid(row=0, column=0, pady=(0, 20))     
+        self.yeast_frame = ctk.CTkFrame(self, fg_color=self.cget("bg"), border_color="white", border_width=1)
+        self.yeast_frame.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+        self.yeast_frame.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
 
-    def button_function(self):
+        yeast_label = ctk.CTkLabel(master=self.yeast_frame, text="Yeast")
+        yeast_label.grid(row=0, column=3, sticky="nsew", pady=5)     
+
+    def button_add_fermentable(self):
+        label = ctk.CTkLabel(master=self.fermentables_frame_scroll, text=self.fermentables_entry.get(), text_color="white")
+        label.grid(row=self.rowno, column=0, sticky="nsew")
+        self.rowno = self.rowno + 1
+
+    def button_add_yeast(self):
         pass
+
+    def button_add_hop(self):
+        pass
+
